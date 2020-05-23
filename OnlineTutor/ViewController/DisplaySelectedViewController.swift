471f7e838486c3da199f457ed1056576ed33cf05
Rojan Bajracharya <rojanbajra25@gmail.com>
2020-05-23 23:03:30 +0545

    view controller created for displaying selected character

---
+++ OnlineTutor/ViewController/DisplaySelectedViewController.swift
@@ -0,0 +1,30 @@
+//
+//  DisplaySelectedViewController.swift
+//  OnlineTutor
+//
+//  Created by Rojan Bajracharya on 5/23/20.
+//  Copyright © 2020 Rojan Bajracharya. All rights reserved.
+//
+
+import UIKit
+
+class DisplaySelectedViewController: UIViewController {
+
+    @IBOutlet weak var lblTitle: UILabel!
+    @IBOutlet weak var txtCharacter: UITextField!
+    @IBOutlet weak var lblCharacter: UILabel!
+    @IBOutlet weak var btnDisplay: UIButton!
+    @IBOutlet weak var btnHowItWorks: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+    @IBAction func btnDisplay(_ sender: Any) {
+    }
+    
+    @IBAction func btnHowItWorks(_ sender: Any) {
+    }
+
+}
