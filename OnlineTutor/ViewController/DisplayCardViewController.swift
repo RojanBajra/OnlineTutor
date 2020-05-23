24d7c4c3b5ec51165b5464dae0e877e3eb5f4e90
Rojan Bajracharya <rojanbajra25@gmail.com>
2020-05-23 18:42:34 +0545

    added display view controller

---
+++ OnlineTutor/ViewController/DisplayCardViewController.swift
@@ -0,0 +1,33 @@
+//
+//  DisplayCardViewController.swift
+//  OnlineTutor
+//
+//  Created by Rojan Bajracharya on 5/23/20.
+//  Copyright © 2020 Rojan Bajracharya. All rights reserved.
+//
+
+import UIKit
+
+class DisplayCardViewController: UIViewController {
+
+    @IBOutlet weak var lblTitle: UILabel!
+    @IBOutlet weak var lblDescription: UILabel!
+    @IBOutlet weak var imgCard: UIImageView!
+    @IBOutlet weak var btnScan: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        setupUI()
+    }
+    
+    func setupUI() {
+        lblTitle.text = "How it works?"
+        lblDescription.text = "Click on the button below to open up your camera. Then show the card that you have and you will see the 3d image of the character. The card must be of the following type."
+        imgCard.image = UIImage.init(named: "two.jpg")
+        btnScan.backgroundColor = ColorForApp.shareInstance.colorPrimary()
+        btnScan.setTitleColor(UIColor.white, for: .normal)
+        
+    }
+
+}
