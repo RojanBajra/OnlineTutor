a826546cb2c5d7df1cffb6e921a442f62921dc05
Rojan Bajracharya <rojanbajra25@gmail.com>
2020-05-24 00:04:56 +0545

    view controller created for Display Selected character page ;

---
+++ OnlineTutor/ViewController/DisplaySelectedARViewController.swift
@@ -0,0 +1,20 @@
+//
+//  DisplaySelectedARViewController.swift
+//  OnlineTutor
+//
+//  Created by Rojan Bajracharya on 5/24/20.
+//  Copyright © 2020 Rojan Bajracharya. All rights reserved.
+//
+
+import UIKit
+import ARKit
+
+class DisplaySelectedARViewController: UIViewController, ARSCNViewDelegate {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+}
