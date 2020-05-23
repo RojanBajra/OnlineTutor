ae4340dcfe06e338d91680b747ebdb9ce4a5eabd
Rojan Bajracharya <rojanbajra25@gmail.com>
2020-05-23 23:18:05 +0545

    struct for pages created

---
+++ OnlineTutor/Model/Pages.swift
@@ -0,0 +1,17 @@
+//
+//  Pages.swift
+//  OnlineTutor
+//
+//  Created by Rojan Bajracharya on 5/23/20.
+//  Copyright © 2020 Rojan Bajracharya. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct Pages {
+    
+    var pageName: String
+    var pageViewController: UIViewController
+
+}
