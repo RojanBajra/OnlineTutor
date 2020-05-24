17125810e1ee8948a971621313755cc5c0ee1b12
Rojan Bajracharya <rojanbajra25@gmail.com>
2020-05-25 00:45:25 +0545

    view controller created for animal page

---
+++ OnlineTutor/ViewController/AnimalViewController.swift
@@ -0,0 +1,30 @@
+//
+//  AnimalViewController.swift
+//  OnlineTutor
+//
+//  Created by Rojan Bajracharya on 5/25/20.
+//  Copyright © 2020 Rojan Bajracharya. All rights reserved.
+//
+
+import UIKit
+
+class AnimalViewController: UIViewController {
+
+    @IBOutlet weak var txtAnimal: UITextField!
+    @IBOutlet weak var btnAnimal: UIButton!
+    @IBOutlet weak var btnCamera: UIButton!
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func btnAnimal(_ sender: Any) {
+    }
+    
+    @IBAction func btnCamera(_ sender: Any) {
+    }
+
+}
