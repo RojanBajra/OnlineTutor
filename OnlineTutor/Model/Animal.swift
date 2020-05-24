82b002e1b5b2063401e02ac1fe264b6e252d1615
Rojan Bajracharya <rojanbajra25@gmail.com>
2020-05-25 00:21:09 +0545

    added the animal struct and animal manager struct

---
+++ OnlineTutor/Model/Animal.swift
@@ -0,0 +1,20 @@
+//
+//  Animal.swift
+//  OnlineTutor
+//
+//  Created by Rojan Bajracharya on 5/25/20.
+//  Copyright © 2020 Rojan Bajracharya. All rights reserved.
+//
+
+import Foundation
+
+struct Animal {
+    
+    var commonName: String
+    var classificationClass: String
+    var phylum: String
+    var scientificName: String
+    var lifeSpan: String
+    var description: String
+    
+}
