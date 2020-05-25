9b0ad18d24b7235d6c0cf28fca738afb44153e65
Rojan Bajracharya <rojanbajra25@gmail.com>
2020-05-26 00:01:07 +0545

    added maths tvc pages

---
+++ OnlineTutor/xibCell/MathsTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  MathsTableViewCell.swift
+//  OnlineTutor
+//
+//  Created by Rojan Bajracharya on 5/26/20.
+//  Copyright © 2020 Rojan Bajracharya. All rights reserved.
+//
+
+import UIKit
+
+class MathsTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
