54e4252910417ec0db6ff2eb6a6f86ee83593c6c
Rojan Bajracharya <rojanbajra25@gmail.com>
2020-05-25 21:53:59 +0545

    added solar system scene page

---
+++ OnlineTutor/SceneViewController/SolarSystemScene.swift
@@ -0,0 +1,9 @@
+//
+//  SolarSystemScene.swift
+//  OnlineTutor
+//
+//  Created by Rojan Bajracharya on 5/25/20.
+//  Copyright © 2020 Rojan Bajracharya. All rights reserved.
+//
+
+import Foundation
