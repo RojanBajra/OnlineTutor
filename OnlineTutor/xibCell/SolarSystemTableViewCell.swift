9533849ce4d938ebaeffb77cf8946662ed29275e
Rojan Bajracharya <rojanbajra25@gmail.com>
2020-05-25 16:08:22 +0545

    solar system table view cell added

---
+++ OnlineTutor/xibCell/SolarSystemTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  SolarSystemTableViewCell.swift
+//  OnlineTutor
+//
+//  Created by Rojan Bajracharya on 5/25/20.
+//  Copyright © 2020 Rojan Bajracharya. All rights reserved.
+//
+
+import UIKit
+
+class SolarSystemTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
