52d077446c85b3c52806bece5983cbd44b84aeb6
Rojan Bajracharya <rojanbajra25@gmail.com>
2020-05-29 02:45:25 +0545

    table view cell added

---
+++ OnlineTutor/xibCell/TestResultTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  TestResultTableViewCell.swift
+//  OnlineTutor
+//
+//  Created by Rojan Bajracharya on 5/29/20.
+//  Copyright © 2020 Rojan Bajracharya. All rights reserved.
+//
+
+import UIKit
+
+class TestResultTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
