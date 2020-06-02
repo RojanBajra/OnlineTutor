2bfb6510a16d37e1cb86ba72f54b715f4a1cd60a
Rojan Bajracharya <rojanbajra25@gmail.com>
2020-06-02 14:33:38 +0545

    changed color

--- OnlineTutor/resources/ColorForApp.swift
+++ OnlineTutor/resources/ColorForApp.swift
@@ -16,7 +16,11 @@ class ColorForApp: NSObject{
 
     func colorPrimary() -> UIColor {
 //        return UIColor.init(red: 90 / 255.0, green: 59 / 255.0, blue: 157 / 255.0, alpha: 1.0)
-        return UIColor.init(red: 211 / 255.0, green: 129 / 255.0, blue: 47 / 255.0, alpha: 1.0)
+//        return UIColor.init(red: 211 / 255.0, green: 129 / 255.0, blue: 47 / 255.0, alpha: 1.0)
+        
+//        return UIColor.init(red: 255 / 255.0, green: 111 / 255.0, blue: 89 / 255.0, alpha: 1.0)
+        
+        return UIColor.init(red: 217 / 255.0, green: 69 / 255.0, blue: 95 / 255.0, alpha: 1.0)
     }
     
     func colorPrimaryHalfAlpha() -> UIColor {
